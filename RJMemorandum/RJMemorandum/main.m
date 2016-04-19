34f177ba7cfca542946eaa98749a4343a643283b
renjialiang <lyc4388@163.com>
2016-04-19 22:55:08 +0800

    init

---
+++ RJMemorandum/RJMemorandum/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RJMemorandum
+//
+//  Created by RenJiaLiang on 16/4/19.
+//  Copyright © 2016年 Renjialiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
